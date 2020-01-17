b0b0ac51b7e5c7aeab7351b2e996b1e83d045238
Karol <karolkolodziej01@gmail.com>
2020-01-17 14:09:10 +0100

    Updates

---
+++ precache-manifest.f9775a5b4853f25f95706a91e87a61b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217c7e645b5150290bbe6086f7a9447b",
+    "url": "/currency_calculator/index.html"
+  },
+  {
+    "revision": "0f865a1f590f9ff9c780",
+    "url": "/currency_calculator/static/css/main.761e0625.chunk.css"
+  },
+  {
+    "revision": "788ebd617612756d5b8f",
+    "url": "/currency_calculator/static/js/2.8baef836.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/currency_calculator/static/js/2.8baef836.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f865a1f590f9ff9c780",
+    "url": "/currency_calculator/static/js/main.240e1b24.chunk.js"
+  },
+  {
+    "revision": "388a80881b245387692f",
+    "url": "/currency_calculator/static/js/runtime-main.e86ffe94.js"
+  }
+]);
